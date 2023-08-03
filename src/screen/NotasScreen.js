bd75bbd202bfb5b8913af225b4c41cd5ae5a5b98
Kambato147 <victordav147@outlook.es>
2023-08-03 17:20:54 -0400

    flashList add

--- src/screen/NotasScreen.js
+++ src/screen/NotasScreen.js
@@ -1,14 +1,41 @@
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 
-import Card from '../components/Trajeta';
+// import Card from '../components/Trajeta';
 
-export default function NotasScreen() {
+// export default function NotasScreen() {
   
+//   return (
+//     <View>
+//       <Text>cmabiar a puros view</Text>
+//     </View>
+//   );
+// };
+
+import { Text } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+
+const DATA = [
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+];
+
+export default function NotasScreen()  {
   return (
-    <View>
-      <Text>cmabiar a puros view</Text>
-    </View>
+    <FlashList
+      numColumns= '2'
+      data={DATA}
+      renderItem={({ item }) => <Text>{item.title}</Text>}
+      estimatedItemSize={200}
+    />
   );
 };
-
-
