39b50f89fada578f445e1d4ce98fc31d927cc44b
victor-romero-martinez <victordav147@gmail.com>
2023-08-06 16:39:25 -0400

    build apk

--- app.config.js
+++ app.config.js
@@ -1,5 +1,3 @@
-import "dotenv/config";
-
 export default {
   expo: {
     name: "notasApp",
@@ -20,14 +18,6 @@ export default {
       },
     },
     extra: {
-      env: {
-        EXPO_PUBLIC_API_KEY: process.env.EXPO_PUBLIC_API_KEY,
-        EXPO_PUBLIC_AUTH_DOMAIN: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-        EXPO_PUBLIC_PROJECT_ID: process.env.EXPO_PUBLIC_PROJECT_ID,
-        EXPO_PUBLIC_STORAGE_BUCKET: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-        EXPO_PUBLIC_MESSAGING_SENDER_ID: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-        EXPO_PUBLIC_APP_ID: process.env.EXPO_PUBLIC_APP_ID,
-      },
       eas: {
         projectId: "dea81c9b-3625-4ea0-b0ca-40383c305389",
       },
